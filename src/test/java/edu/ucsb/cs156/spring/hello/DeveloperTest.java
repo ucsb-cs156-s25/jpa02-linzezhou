e718367ad79d112e5ee4882695d51f4190aa37c1
Forrest Zhou <lzhou@ucsb.edu>
2025-04-07 22:30:17 -0700

    xy - updated with actual developer name

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -22,8 +22,6 @@ public class DeveloperTest {
 
     @Test
     public void getName_returns_correct_name() {
-        // TODO: Replace Chris G. with your name as shown on
-        // <https://bit.ly/cs156-s25-teams>
         assertEquals("Forrest Z", Developer.getName());
     }
 
